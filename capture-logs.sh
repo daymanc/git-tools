c28f737566631bc57c9f5760462901293397ed77
daymancc <dayman@pistoncloud.com>
2014-07-15 11:29:26 -0700

    Adding support for upgrade tests. Better verbosity for log filenames. Cleaned up original code a bit.

--- capture-logs.sh
+++ capture-logs.sh
@@ -4,27 +4,57 @@ set -x
 set -e
 set -o pipefail
 
-if [ -z "$FILESERVER" -o -z "$FS_SSH_USER" -o -z "$RELEASE_VERSION" -o -z "$GITHUB_OWNER" -o -z "$GITHUB_BRANCH" -o -z "$BUILD_NUMBER" -o -z "$WORKSPACE" ]
+FS_SSH_USER="builder"
+JENKINS_CLUSTER="20"
+FILESERVER="smurf"
+
+env | sort
+
+if [ -z "${FILESERVER}" -o -z "${FS_SSH_USER}" -o -z "${RELEASE_VERSION}" -o -z "${GITHUB_OWNER}" -o -z "${GITHUB_BRANCH}" -o -z "${BUILD_NUMBER}" -o -z "${WORKSPACE}" ]
 then
     echo "Required variable not set" >&2
     exit 1
 fi
 
-RELEASE_VERSION_ESCAPED=$(echo "$RELEASE_VERSION" | sed -e 's|\.|\\\.|g')
+# If we have a source version and a release version then we should be part of an upgrade
+[ "${SOURCE_VERSION}" ] && UPGRADE=1
 
-nc -w0 -u $FILESERVER 514 <<< "automated-build20: Build $RELEASE_VERSION ($BUILD_NUMBER) finish"
+# If not defined, then we are bound to the ever-locked Jenkins cluster 20
+CLUSTER=${CLUSTER:-${JENKINS_CLUSTER}}
+BUILD_STRING="automated-build${CLUSTER}"
+CLUSTER_LOGFILE="${CLUSTER}-messages"
+AUTOMATED_BUILD_GZIP="automated-build.log.gz"
 
-ssh $FS_SSH_USER@$FILESERVER "zcat /var/log/20-messages.1.gz | cat - /var/log/20-messages | awk '/ automated-build20: Build $RELEASE_VERSION_ESCAPED \($BUILD_NUMBER\) start/,/ automated-build20: Build $RELEASE_VERSION_ESCAPED \($BUILD_NUMBER\) finish$/' | gzip -c > automated-build.log.gz"
+SOURCE_VERSION_ESCAPED=$(echo "${SOURCE_VERSION}" | sed -e 's|\.|\\\.|g')
+RELEASE_VERSION_ESCAPED=$(echo "${RELEASE_VERSION}" | sed -e 's|\.|\\\.|g')
+
+# Push in a finish delimiter to the finished job
+nc -w0 -u ${FILESERVER} 514 <<< "${BUILD_STRING}: Build ${RELEASE_VERSION} (${BUILD_NUMBER}) finish"
+
+if [ "${UPGRADE}" ]
+then
+    echo "NOTE: This was an Upgrade test."
+    RESULTS_LOG="upgrade-test-${SOURCE_VERSION}-to-${RELEASE_VERSION}-testid-${BUILD_NUMBER}"
+    TEACUP_ARTIFACTS_DIR="teacup-artifacts-${SOURCE_VERSION}-to-${RELEASE_VERSION}-testid-${BUILD_NUMBER}"
+
+    ssh ${FS_SSH_USER}@${FILESERVER} "zcat /var/log/${CLUSTER_LOGFILE}.1.gz | cat - /var/log/${CLUSTER_LOGFILE} | awk '/ ${BUILD_STRING}: Build ${SOURCE_VERSION_ESCAPED} \(${BUILD_NUMBER}\) start FLAVOR=pentos/,/ ${BUILD_STRING}: Build ${RELEASE_VERSION_ESCAPED} \(${BUILD_NUMBER}\) finish$/' | gzip -c > ${AUTOMATED_BUILD_GZIP}"
+else
+    RESULTS_LOG="functional-test-${RELEASE_VERSION}-testid-${BUILD_NUMBER}"
+    TEACUP_ARTIFACTS_DIR="teacup-artifacts-${RELEASE_VERSION}-testid-${BUILD_NUMBER}"
+
+    ssh ${FS_SSH_USER}@${FILESERVER} "zcat /var/log/${CLUSTER_LOGFILE}.1.gz | cat - /var/log/${CLUSTER_LOGFILE} | awk '/ ${BUILD_STRING}: Build ${RELEASE_VERSION_ESCAPED} \(${BUILD_NUMBER}\) start/,/ ${BUILD_STRING}: Build ${RELEASE_VERSION_ESCAPED} \(${BUILD_NUMBER}\) finish$/' | gzip -c > ${AUTOMATED_BUILD_GZIP}"
+fi
 
 # TODO(NB) Try and do this in one step, the above command should be able to copy it directly, but ssh -A isn't working for some reason.
-scp -q $FS_SSH_USER@$FILESERVER:automated-build.log.gz .
+scp -q ${FS_SSH_USER}@${FILESERVER}:${AUTOMATED_BUILD_GZIP} .
 
-scp -q automated-build.log.gz $FS_SSH_USER@$FILESERVER:/home/shared/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/functional-test-$RELEASE_VERSION-$BUILD_NUMBER.log.gz
+scp -q automated-build.log.gz ${FS_SSH_USER}@${FILESERVER}:/home/shared/builds/${GITHUB_OWNER}/${GITHUB_BRANCH}/debug/${RESULTS_LOG}.log.gz
 
-if [ -d /tmp/teacup-artifacts-$EXECUTOR_NUMBER ]
+if [ -d /tmp/teacup-artifacts-${EXECUTOR_NUMBER} ]
 then
-    rsync -rltgoD --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r /tmp/teacup-artifacts-$EXECUTOR_NUMBER/ $FS_SSH_USER@$FILESERVER:/home/shared/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/teacup-artifacts-$RELEASE_VERSION-$BUILD_NUMBER/
-    rm -rf /tmp/teacup-artifacts-$EXECUTOR_NUMBER
+    rsync -rltgoD --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r /tmp/teacup-artifacts-${EXECUTOR_NUMBER}/ ${FS_SSH_USER}@${FILESERVER}:/home/shared/builds/${GITHUB_OWNER}/${GITHUB_BRANCH}/debug/${TEACUP_ARTIFACTS_DIR}/
+    # this is already run above, so this file won't appear. will appear if only run once
+    rm -rf /tmp/teacup-artifacts-${EXECUTOR_NUMBER}
 fi
 
 set +x
@@ -33,9 +63,9 @@ echo
 echo
 echo "Errors in the log:"
 
-if test -x "$WORKSPACE"/teacup/tools/extract-errors.py
+if [ -x "${WORKSPACE}"/teacup/tools/extract-errors.py ]
 then
-    zcat automated-build.log.gz | "$WORKSPACE"/teacup/tools/extract-errors.py
+    zcat automated-build.log.gz | "${WORKSPACE}"/teacup/tools/extract-errors.py
 else
     zcat automated-build.log.gz | awk '/^Traceback \(most recent/,/^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/'
 fi
@@ -43,9 +73,8 @@ echo
 
 cat <<EOF
 Options to view the log:
-http://$FILESERVER/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/functional-test-$RELEASE_VERSION-$BUILD_NUMBER.log
-http://$FILESERVER/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/functional-test-$RELEASE_VERSION-$BUILD_NUMBER.log.gz
-http://$FILESERVER/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/teacup-artifacts-$RELEASE_VERSION-$BUILD_NUMBER/
-scp $FILESERVER:/home/shared/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/functional-test-$RELEASE_VERSION-$BUILD_NUMBER.log.gz .
-
+http://${FILESERVER}/builds/${GITHUB_OWNER}/${GITHUB_BRANCH}/debug/${RESULTS_LOG}.log
+http://${FILESERVER}/builds/${GITHUB_OWNER}/${GITHUB_BRANCH}/debug/${RESULTS_LOG}.log.gz
+http://${FILESERVER}/builds/${GITHUB_OWNER}/${GITHUB_BRANCH}/debug/${TEACUP_ARTIFACTS_DIR}/
+scp ${FILESERVER}:/home/shared/builds/${GITHUB_OWNER}/${GITHUB_BRANCH}/debug/${RESULTS_LOG}.log.gz .
 EOF
